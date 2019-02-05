42289e2b2dfb4c31c41eeb84e65e66e37987a5ae
Yuretzz <yuvede@gmail.com>
2019-02-05 09:50:02 +0300

    Added shalom /name endpoint

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/<name>')
+def hello_name(name):
+    return f"Shalom, {name}!"
+
 
 if __name__ == '__main__':
     app.run()
